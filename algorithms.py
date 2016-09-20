bdab3e1df7f250e56eb50296fc39bf42f09b6e4b
tdiede <tdiede@gmail.com>
2016-09-19 23:41:29 -0700

    Solved three easy whiteboarding challenges.

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,52 @@
 # These are the coding challenges I have worked through.
 
 
+def lucky_numbers(n):
+    """Return n unique random numbers from 1-10 (inclusive).
+        >>> lucky_numbers(2)
+        [3, 7]
+
+        >>> lucky_numbers(0)
+        []
+
+        >>> sorted(lucky_numbers(10))
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+
+    import random
+
+    nums = range(1, 11)
+    lucky_nums = []
+
+    for i in range(n):
+        num = random.choice(nums)
+        nums.remove(num)
+        lucky_nums.append(num)
+
+    return lucky_nums
+
+
+def concat_lists(list1, list2):
+    """Combine lists.
+        >>> concat_lists([1, 2], [3, 4])
+        [1, 2, 3, 4]
+
+        >>> concat_lists([], [1, 2])
+        [1, 2]
+
+        >>> concat_lists([1, 2], [])
+        [1, 2]
+
+        >>> concat_lists([], [])
+        []
+    """
+
+    for item in list2:
+        list1.append(item)
+
+    return list1
+
+
 def add_to_zero(nums):
     """Given list of ints, return True if any two nums in list sum to 0.
         >>> add_to_zero([])
