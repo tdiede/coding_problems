f579723e1dfaf9c6c186b5e3e42a75fde046a63e
Therese Diede <tdiede@gmail.com>
2016-09-29 03:51:38 +0000

    day 10

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,327 @@
 # These are the coding challenges I have worked through.
 
 
+# medium, below
+
+# # Concepts: Dictionaries, General
+# def is_anagram_of_palindrome(word):
+#     """Is a word an anagram of a palindrome?
+#         >>> is_anagram_of_palindrome("a")
+#         True
+
+#         >>> is_anagram_of_palindrome("ab")
+#         False
+
+#         >>> is_anagram_of_palindrome("aab")
+#         True
+
+#         >>> is_anagram_of_palindrome("arceace")
+#         True
+
+#         >>> is_anagram_of_palindrome("arceaceb")
+#         False
+#     """
+
+#     # want to count whether each letter appears an even number of times
+#     # one letter can appear an odd number of times
+
+#     # seen = []
+
+#     # i = 0
+#     # for i in range(len(word)):
+#     #     if word[i] not in seen:
+#     #         seen.append(word[i])
+#     #     else:
+#     #         seen.remove(word[i])
+
+#     # if len(seen) <= 1:
+#     #     return True
+
+#     # return False
+
+#     seen = {}
+
+#     i = 0
+#     for i in range(len(word)):
+#         count = seen.get(word[i], 0) + 1
+#         seen[word[i]] = count
+
+#     seen_odd = False
+
+#     for value in seen.itervalues():
+#         if value % 2 != 0:
+#             if seen_odd:
+#                 return False
+#             seen_odd = True
+
+#     return True
+
+
+# # Binary Search
+# def binary_search(val):
+#     """Using binary search, find val in range 1-100. Return # of guesses.
+#         >>> binary_search(50)
+#         1
+
+#         >>> binary_search(25)
+#         2
+
+#         >>> binary_search(75)
+#         2
+
+#         >>> binary_search(31) <= 7
+#         True
+
+#         >>> max([binary_search(i) for i in range(1, 101)])
+#         7
+#     """
+
+#     assert 0 < val < 101, "Val must be between 1-100"
+
+#     num_guesses = 0
+
+#     return num_guesses
+
+
+# Count List Recursively
+# Count the number of items in a list using recursion.
+# Concepts: Recursion
+
+# Decode a String
+# Decode a string into the original text.
+# Concepts: Loops
+
+# Missing Number
+# Find the missing number in a list.
+# Concepts: Runtime, General
+
+# Print Digits Backwards
+# Print digits of a number on backwards order.
+# Concepts: General, Math
+
+# Concepts: Recursion
+def print_recursively(lst):
+    """Print items in a list using recursion.
+        >>> print_recursively([1, 2, 3])
+        1
+        2
+        3
+    """
+
+    if lst:
+        print lst[0]
+        print_recursively(lst[1:])
+
+
+print_recursively([1, 2, 3])
+
+
+# Concepts: Recursion
+def recursive_index(needle, haystack):
+    """Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+        >>> lst = ['hey', 'there', 'you']
+        >>> recursive_index('hey', lst)
+        0
+        >>> recursive_index('you', lst)
+        2
+        >>> recursive_index('porcupine', lst) is None
+        True
+    """
+
+    def _recursive_index(needle, haystack, idx):
+
+        if idx == len(haystack):
+            return None
+
+        if haystack[idx] == needle:
+            return idx
+
+        return _recursive_index(needle, haystack, idx + 1)
+
+    return _recursive_index(needle, haystack, 0)
+
+
+# Remove Linked List Node
+# Remove a node from the start/middle of a linked list.
+# Concepts: Linked Lists
+
+# Reverse Linked List
+# Reverse a linked list, returning a new list.
+# Concepts: Linked Lists
+
+# Sort Sorted Lists
+# Merge together two already-sorted lists.
+# Concepts: Loops, Sorting
+
+
+# Concepts: Loops, Strings
+def split_string(string, delimiter):
+    """Split a string on another, like the Python built-in split.
+        >>> x
+    """
+
+    out = []
+    prev = 0
+
+    for idx, char in enumerate(string+delimiter):
+        if char == delimiter:
+            split_string.append(string[prev:idx])
+            prev = idx
+
+    # split_string.append(string[prev:])
+
+    return split_string
+
+
+# easier, below
+
+def sum_list(num_list):
+    """Return the sum of all numbers in list.
+        >>> sum_list([5, 3, 6, 2, 1])
+        17
+    """
+
+    sum_num = 0
+
+    for num in num_list:
+        sum_num += num
+
+    return sum_num
+
+
+def show_evens(nums):
+    """Given list of ints, return list of *indices* of even numbers in list.
+        >>> lst = [1, 2, 3, 4, 6, 8]
+        >>> show_evens(lst)
+        [1, 3, 4, 5]
+    """
+
+    evens = []
+    i = 0
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            evens.append(i)
+
+    return evens
+
+
+def rev_string(astring):
+    """Return reverse of string.
+        >>> rev_string("porcupine")
+        'enipucrop'
+    """
+
+    i = 0
+    rev_string = ""
+    for i in range(len(astring)):
+        rev_string += astring[-1-i]
+
+    return rev_string
+
+
+def rev_list_in_place(lst):
+    """Reverse list in place.
+        >>> lst = [1, 2, 3]
+        >>> rev_list_in_place(lst)
+        >>> lst
+        [3, 2, 1]
+    """
+
+    i = 0
+    for i in range(len(lst)/2):
+        lst[i], lst[-1-i] = lst[-1-i], lst[i]
+
+
+def pig_latin(phrase):
+    """Turn a phrase into pig latin.
+        >>> pig_latin('hello awesome programmer')
+        'ellohay awesomeyay rogrammerpay'
+
+        >>> pig_latin('porcupine are cute')
+        'orcupinepay areyay utecay'
+
+        >>> pig_latin('give me an apple')
+        'ivegay emay anyay appleyay'
+    """
+
+    pig_latin_string = ""
+    phrase = phrase.split(" ")
+
+    for word in phrase:
+
+        pig_latin_word = ""
+
+        if word[0] not in ['a', 'e', 'i', 'o', 'u']:
+            pig_latin_word = word[1:] + word[0] + 'ay' + ' '
+        else:
+            pig_latin_word = word + 'yay' + ' '
+
+        pig_latin_string += pig_latin_word
+
+    return pig_latin_string.rstrip(' ')
+
+
+def max_of_three(num1, num2, num3):
+    """Returns the largest of three integers.
+        >>> max_of_three(1, 5, 2)
+        5
+
+        >>> max_of_three(10, 1, 11)
+        11
+    """
+
+    if num1 >= num2 and num1 >= num3:
+        return num1
+
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+
+    elif num3 >= num1 and num3 >= num2:
+        return num3
+
+
+def max_num(num_list):
+    """Returns largest integer from given list.
+        >>> max_num([5, 3, 6, 2, 1])
+        6
+    """
+
+    max_num = num_list[0]
+
+    for num in num_list:
+        if num > max_num:
+            max_num = num
+
+    return max_num
+
+
+def is_palindrome(word):
+    """Return True/False if this word is a palindrome.
+        >>> is_palindrome("a")
+        True
+
+        >>> is_palindrome("noon")
+        True
+
+        >>> is_palindrome("racecar")
+        True
+
+        >>> is_palindrome("porcupine")
+        False
+
+        >>> is_palindrome("Racecar")
+        False
+    """
+
+    for i in range(len(word)/2):
+        if word[i] != word[-1-i]:
+            return False
+
+    return True
+
+
 def is_prime(num):
     """Is a number a prime number?
         >>> is_prime(0)
--- hackerrank.py
+++ hackerrank.py
@@ -1,21 +1,16 @@
 import sys
 
 
-# # basic read/write
+# # DAY 0: BASIC READ/WRITE
 # print 'enter text you want to echo.'
 # s = str(sys.stdin.readline())
 # sys.stdout.write(s)
 
 
-# # # data types
+# # DAY 1: DATA TYPES
 # i = 4
 # d = 4.0
 # s = 'HackerRank '
-
-# # # Declare second integer, double, and String variables.
-# # i2 = int()
-# # d2 = float()
-# # s2 = str()
 
 # # Read and save an integer, double, and String to your variables.
 # print i
@@ -37,6 +32,7 @@ import sys
 # sys.stdout.writelines('%s' % (s+s2))
 
 
+# # DAY 2: OPERATORS
 # # enter three lines: meal cost, tip % and tax %
 # print 'enter three lines: meal cost as float, tip and tax as integers percent.'
 # meal_cost = float(sys.stdin.readline())
@@ -48,9 +44,9 @@ import sys
 # sys.stdout.write('The total meal cost is %d dollars. \n' % round(total_cost))
 
 
+# # DAY 3: CONDITIONAL STATEMENTS
 # # performs a conditional operation on given integer input
 # N = int(raw_input('Enter an integer. >').strip())
-
 
 # def conditional(n):
 
@@ -67,7 +63,7 @@ import sys
 # conditional(N)
 
 
-# # class vs. instance
+# # DAY 4: CLASS VS. INSTANCE
 # class Person:
 #     def __init__(self, initialAge):
 #         # Add some more code to run some checks on initialAge
@@ -92,7 +88,6 @@ import sys
 #         # Increment the age of the person in here
 #         self.age += 1
 
-
 # t = int(raw_input())
 # for i in range(0, t):
 #     age = int(raw_input())
@@ -104,7 +99,7 @@ import sys
 #     print('')
 
 
-# # loops
+# # DAY 5: LOOPS
 # N = int(raw_input().strip())
 
 # i = 1
@@ -113,56 +108,78 @@ import sys
 #     sys.stdout.write('%i x %i = %i \n' % (N, i, multiple))
 
 
-# review loops (string)
-count = int(sys.stdin.readline())
+# # DAY 6: REVIEW
+# count = int(sys.stdin.readline())
 
-for line in range(count):
-    s = str(sys.stdin.readline()).strip()
-    sys.stdout.write('%s %s \n' % ("".join(s[::2]), "".join(s[1::2])))
-
-
-# arrays
-n = int(raw_input().strip())
-arr = map(int, raw_input().strip().split(' '))
-
-seq = ' '.join(map(str, arr[::-1]))
-
-sys.stdout.write('%s' % seq)
+# for line in range(count):
+#     s = str(sys.stdin.readline()).strip()
+#     sys.stdout.write('%s %s \n' % ("".join(s[::2]), "".join(s[1::2])))
 
 
-# dictionaries/maps/hashmaps
-n = int(sys.stdin.readline())
+# # DAY 7: ARRAYS
+# n = int(raw_input().strip())
+# arr = map(int, raw_input().strip().split(' '))
 
-phonebook = {}
+# seq = ' '.join(map(str, arr[::-1]))
 
-i = 0
-for entry in range(i, n):
-    entry = sys.stdin.readline()
-    entry_item = entry.strip().split(' ')
-    phonebook[entry_item[0]] = entry_item[1]
-    # sys.stdout.write('%s' % phonebook)
-
-loop = True
-while loop is True:
-    query = sys.stdin.readline()
-    if query:
-        name = query.strip()
-        number = phonebook.get(name, 'Not found')
-        if name in phonebook:
-            sys.stdout.write('%s=%s \n' % (name, number))
-        else:
-            sys.stdout.write('%s \n' % (number))
-    else:
-        loop = False
+# sys.stdout.write('%s' % seq)
 
 
-# recursion
-def factorial(n):
-    if n == 0:
-        return 1
+# # DAY 8: DICTIONARIES AND MAPS
+# n = int(sys.stdin.readline())
 
-    return n * factorial(n-1)
+# phonebook = {}
 
-N = int(sys.stdin.readline())
-factorial = factorial(N)
-sys.stdout.write('%i' % factorial)
+# i = 0
+# for entry in range(i, n):
+#     entry = sys.stdin.readline()
+#     entry_item = entry.strip().split(' ')
+#     phonebook[entry_item[0]] = entry_item[1]
+#     # sys.stdout.write('%s' % phonebook)
+
+# loop = True
+# while loop is True:
+#     query = sys.stdin.readline()
+#     if query:
+#         name = query.strip()
+#         number = phonebook.get(name, 'Not found')
+#         if name in phonebook:
+#             sys.stdout.write('%s=%s \n' % (name, number))
+#         else:
+#             sys.stdout.write('%s \n' % (number))
+#     else:
+#         loop = False
+
+
+# # DAY 9: RECURSION
+# def factorial(n):
+#     if n == 0:
+#         return 1
+
+#     return n * factorial(n-1)
+
+# N = int(sys.stdin.readline())
+# factorial = factorial(N)
+# sys.stdout.write('%i' % factorial)
+
+
+# # DAY 10: BINARY NUMBERS
+# n = int(raw_input().strip())
+
+# count = 0
+# max_count = 0
+
+# while(n > 0):
+#     if (n % 2 == 1):
+#         count += 1
+#         if count > max_count:
+#             max_count = count
+#         n = n/2
+#     else:
+#         count = 0
+#         n = n/2
+
+# sys.stdout.write('%i' % max_count)
+
+
+# # DAY 11: 2D ARRAYS
